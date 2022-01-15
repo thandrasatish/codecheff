3e430f9243c47df005ea9690b8b0f3ca9ec10fad
thandrasatish <61319878+thandrasatish@users.noreply.github.com>
2022-01-15 22:27:58 +0530

    Chef and SnackDown.py

---
+++ practice/Chef and SnackDown.py
@@ -0,0 +1,9 @@
+for i in range(int(input())):
+    a=int(input())
+    if(a==2010  or a==2015 or a==2016 or a==2017 or a==2019):
+        print("HOSTED")
+    else:
+        print("NOT HOSTED")
+    
+    
+  
