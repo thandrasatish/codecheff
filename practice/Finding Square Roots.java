e1bc7643a3ca7a93ffdeed743ec436191cfa5cba
thandrasatish <61319878+thandrasatish@users.noreply.github.com>
2022-01-15 22:04:31 +0530

    Finding Square Roots.java

---
+++ practice/Finding Square Roots.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Satish
+{
+    public static void main(String[] args)
+    {
+        int n,i;
+        Scanner s= new Scanner(System.in);
+        n=s.nextInt();
+        int a[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            System.out.println((int)Math.sqrt(a[i]));
+        }
+    }
+}
