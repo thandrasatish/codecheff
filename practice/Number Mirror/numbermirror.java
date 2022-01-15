2e12792bb9f09a89afe1ace069f8ba37c7d86084
thandrasatish <61319878+thandrasatish@users.noreply.github.com>
2022-01-15 19:54:45 +0530

    numbermirror.java

---
+++ practice/Number Mirror/numbermirror.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    { 
+        int n;
+        Scanner s = new Scanner(System.in);
+        n=s.nextInt();
+        System.out.println(n);
+        
+    }
+    
+}
